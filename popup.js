64c4a6206b3752777e733c45978ceda255a4fc60
TumGuTum <150485499+TumGuTum@users.noreply.github.com>
2024-04-19 15:35:13 +0300

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,40 @@
+function showPopup() {
+    document.getElementById("popup-container").style.display = "block";
+}
+
+function hidePopup() {
+    document.getElementById("popup-container").style.display = "none";
+}
+function openPopup(title, imageUrl) {
+    var popup = document.getElementById('popup');
+    var courseTitle = document.getElementById('course-title');
+    var courseImage = document.getElementById('course-image');
+      // Добавляем эффект размытия к основной странице
+  document.body.insertAdjacentHTML('beforeend', '<div class="popup-overlay"></div>');
+  
+    courseTitle.textContent = title;
+    courseImage.src = imageUrl;
+  
+    popup.style.display = 'block';
+  
+    // Pause video if it's playing
+    var videos = document.querySelectorAll('video');
+    videos.forEach(function(video) {
+      if (!video.paused) {
+        video.pause();
+      }
+    });
+  }
+  
+  function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
+    document.querySelector('.popup-overlay').remove();
+    // Pause video if it's playing
+    var videos = document.querySelectorAll('video');
+    videos.forEach(function(video) {
+      if (!video.paused) {
+        video.pause();
+      }
+    });
+  }
